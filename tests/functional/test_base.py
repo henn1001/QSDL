223842a0e314940e6f65903b9ad723fb3aa4349d
henn1001 <henn1001@outlook.com>
2020-08-16 16:31:06 +0200

    test: move test helper functions to parent folder and add two basic unit tests

--- tests/functional/test_base.py
+++ tests/functional/test_base.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from . import wrapper_generate
-from . import wrapper_generate_failure
+from tests import wrapper_generate
+from tests import wrapper_generate_failure
 
 
 class Testbase:
